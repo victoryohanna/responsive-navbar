dc7f34d43c8af9b6368d1145cbb172cd6d23d3fa
Victor Yohanna <victoryohanna@gmail.com>
2022-06-15 14:45:07 +0100

    modified Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='__navbar'>
+        <div className='__navbar-links'>
+            <div className='__navbaar-links_logo'>
+                <img src="" alt="" />
+            </div>
+        </div>
+    </div>
   )
 }
 
