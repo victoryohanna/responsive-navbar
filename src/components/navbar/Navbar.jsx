c9df9aea8cf88ed9d786f94107fd93bf0d48d624
Victor Yohanna <victoryohanna@gmail.com>
2022-06-15 15:23:34 +0100

    add logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+import logo from '../../assets/logo.png';
+import "./navbar.css";
 
 function Navbar() {
   return (
     <div className='__navbar'>
         <div className='__navbar-links'>
             <div className='__navbaar-links_logo'>
-                <img src="" alt="" />
+                <img src={logo} alt="Logo" />
             </div>
         </div>
     </div>
