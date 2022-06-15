dd83993ebc9c9d52f210a4025011bf6d2900e7a4
Victor Yohanna <victoryohanna@gmail.com>
2022-06-15 16:17:42 +0100

    add navbar links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,6 +10,16 @@ function Navbar() {
             <div className='__navbaar-links_logo'>
                 <img src={logo} alt="Logo" />
             </div>
+            <div className='__navbar-links_container'>
+                <p><a href="#home">Home</a></p>
+                <p><a href="#about">About</a></p>
+                <p><a href="#blogs">blogs</a></p>
+                <p><a href="#contact">Contact</a></p>
+            </div>
+        </div>
+        <div className='__navbar-sign'>
+            <p>sign in</p>
+            <button type='button'>sign up</button>
         </div>
     </div>
   )
